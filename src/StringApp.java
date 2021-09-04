ed18ffdad086b8cc405c2277888891a120a4e917
hiyee <gyeongjae.h.dev@gmail.com>
2021-09-04 23:15:57 +0900

    First push.

---
+++ src/StringApp.java
@@ -0,0 +1,6 @@
+public class StringApp {
+    public static void main(String[] args) {
+        System.out.println("hello world"); // String
+        System.out.println('h'); // Character 한 글자를 나타냄
+    }
+}
