a2ed580104d40961ba257edb90bdd1d4e17ddece
hiyee <gyeongjae.h.dev@gmail.com>
2021-09-05 12:19:25 +0900

    Practice Java basic feature and debug mode.

--- src/StringApp.java
+++ src/StringApp.java
@@ -2,5 +2,7 @@ public class StringApp {
     public static void main(String[] args) {
         System.out.println("hello world"); // String
         System.out.println('h'); // Character 한 글자를 나타냄
+
+        System.out.println("Hello\nWorld");
     }
 }
---
+++ src/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main(String[] args) {
+        int a = 1; // Number -> integer ... -2, -1, 1, ...
+        System.out.println(a);
+
+        double b = 1.1; // real number -> double ... -2.0, -1.0 ...
+        System.out.println(b);
+
+        String c = "Hello World";
+        System.out.println(c);
+
+        // 타입을 지정해주면 다른 값이 들어갈 일이 없기 때문에 확신을 가질 수 있다는 장점
+    }
+}
