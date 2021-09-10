ff0b3c950a506f74e5baf5cf061652aab5ad7f75
hiyee-gj <gyeongjae.h.dev@gmail.com>
2021-09-10 16:30:48 +0900

    Added javac class.

--- src/OkJavaGoInHomeinput.java
+++ src/OkJavaGoInHomeinput.java
@@ -6,9 +6,11 @@ import org.opentutorials.iot.Security;
 import javax.swing.*;
 
 public class OkJavaGoInHomeinput {
+
+    //parameter, 매개변수
     public static void main(String[] args) {
-        String id = JOptionPane.showInputDialog("Enter a ID");
-        String bright = JOptionPane.showInputDialog("Enter a Bright level");
+        String id = args[0];
+        String bright = args[1];
 
         //Elevator call
         Elevator myElevator = new Elevator(id);
