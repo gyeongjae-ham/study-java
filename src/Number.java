9cbaa819f577f821b1b451f03a9dd7e45dedec84
GyeongJae <gyeongjae.h.dev@gmail.com>
2021-09-11 19:02:16 +0900

    ADD: java practice file.

--- src/Number.java
+++ src/Number.java
@@ -1,13 +1,18 @@
 public class Number {
     public static void main(String[] args) {
-        // Operator
-        System.out.println(6 + 2); // 8
-        System.out.println(6-2); // 4
-        System.out.println(6*2); // 12
-        System.out.println(6/2); // 3
+        byte byteInteger = 10;
+        short shortInteger = 20;
+        int intInteger = 30;
+        long longInteger = 40;
 
-        System.out.println(Math.PI); // 파이
-        System.out.println(Math.floor(Math.PI));
-        System.out.println(Math.ceil(Math.PI));
+        System.out.println(byteInteger);
+        System.out.println(shortInteger);
+        System.out.println(intInteger);
+        System.out.println(longInteger);
+
+        int sum = byteInteger + shortInteger;
+        int sum2 = shortInteger + intInteger;
+        long sum3 = intInteger + longInteger;
+        long sum4 = byteInteger + longInteger;
     }
 }
