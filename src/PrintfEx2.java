40b2274780915f32a60a259c5930846ba6ec363e
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-01-31 22:38:47 +0900

    Added printfEx2

---
+++ src/PrintfEx2.java
@@ -0,0 +1,10 @@
+public class PrintfEx2 {
+    public static void main(String[] args) {
+        String url = "www.codechobo.com";
+
+        float f1 = .10f; // 0.10, 1.0e-1
+        float f2 = 1e1f; // 10.0, 1.0eq, 1.0e+1
+        float f3 = 3.14e3f;
+        double d = 1.234456789;
+    }
+}
