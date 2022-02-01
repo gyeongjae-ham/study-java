c2da9156ab119c00efa2716f3982ab82a4125f60
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-02-01 21:35:11 +0900

    Added printfEx2

--- src/PrintfEx2.java
+++ src/PrintfEx2.java
@@ -6,5 +6,8 @@ public class PrintfEx2 {
         float f2 = 1e1f; // 10.0, 1.0eq, 1.0e+1
         float f3 = 3.14e3f;
         double d = 1.234456789;
+
+        System.out.printf("f1=%f, %e, %g%n", f1, f1, f1);
+
     }
 }
