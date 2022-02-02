ef93b8708e26c92eec21ce6881476f57580154e0
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-02-02 19:07:55 +0900

    Added printfEx2

--- src/PrintfEx2.java
+++ src/PrintfEx2.java
@@ -8,6 +8,8 @@ public class PrintfEx2 {
         double d = 1.234456789;
 
         System.out.printf("f1=%f, %e, %g%n", f1, f1, f1);
+        System.out.printf("f2=%f, %e, %g%n", f2, f2, f2);
+        System.out.printf("f3=%f, %e, %g%n", f3, f3, f3);
 
     }
 }
