a3ab3e41f6840a2bdf761b282a60258292522995
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-02-24 14:18:14 +0900

    FEAT: OperatorEx2 작성

---
+++ src/OperatoreEx2.java
@@ -0,0 +1,23 @@
+public class OperatoreEx2 {
+    public static void main(String[] args) {
+        char c = 'a';
+        for (int i = 0; i < 26; i++) {
+            System.out.print(c++);
+        }
+
+        System.out.println(); // 줄바꿈을 한다
+
+        c = 'A';
+        for (int i = 0; i < 26; i++) {
+            System.out.print(c++);
+        }
+
+        System.out.println();
+
+        c = '0';
+        for (int i = 0; i < 10; i++) {
+            System.out.print(c++);
+        }
+        System.out.println();
+    }
+}
