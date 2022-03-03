50f1611361d61759ae328911a2decbe82a343107
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-03-03 17:31:14 +0900

    :recycle: Modify Class Name
    중복 이름 수정

---
+++ src/chapter/seventh/PolyArgumentTest.java
@@ -0,0 +1,9 @@
+package chapter.seventh;
+
+class Product {
+    int price; // 제품의 가격
+    int bonusPoint; // 제품 구매 시 제공하는 보너스 점수
+}
+
+public class PolyArgumentTest {
+}
--- src/chapter/seventh/ProductTest.java
+++ src/chapter/seventh/ProductTest.java
@@ -1,6 +1,6 @@
 package chapter.seventh;
 
-class Product {
+class ProductInfo {
     static int count = 0;
     int serialNo;
 
@@ -9,18 +9,18 @@ class Product {
         serialNo = count;
     }
 
-    public Product() {} // 기본 생성자, 생략 가능
+    public ProductInfo() {} // 기본 생성자, 생략 가능
 }
 
 public class ProductTest {
     public static void main(String[] args) {
-        Product p1 = new Product();
-        Product p2 = new Product();
-        Product p3 = new Product();
+        ProductInfo p1 = new ProductInfo();
+        ProductInfo p2 = new ProductInfo();
+        ProductInfo p3 = new ProductInfo();
 
         System.out.println("p1의 제품번호(serial no)는 " + p1.serialNo);
         System.out.println("p2의 제품번호(serial no)는 " + p1.serialNo);
         System.out.println("p3의 제품번호(serial no)는 " + p1.serialNo);
-        System.out.println("생산된 제품의 수는 모두 " + Product.count + "개 입니다.");
+        System.out.println("생산된 제품의 수는 모두 " + ProductInfo.count + "개 입니다.");
     }
 }
